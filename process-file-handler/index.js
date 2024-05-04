7b42104eb1312718b4272107638cf924f9e707a7
rajesh <rajesh.chaudhary786@gmail.com>
2024-05-05 02:28:27 +0530

    Added sqs

--- process-file-handler/index.js
+++ process-file-handler/index.js
@@ -1,14 +1,23 @@
 const AWS = require('aws-sdk');
+const { BatchProcessor, EventType, processPartialResponse } = require("@aws-lambda-powertools/batch");
 const s3 = new AWS.S3();
+const processor = new BatchProcessor(EventType.SQS);
 
 exports.handler = async (event) => {
     console.log(JSON.stringify(event));
-    const bucketName = process.env.BUCKET_NAME;
-    const key = event.Records[0].s3.object.key;
+   return await processPartialResponse(event, recordHandler, processor);
+}
 
-    console.log(`bucketName: ${bucketName} and key: ${key}`);
+const recordHandler = async (record) => {
+    const bucketName = process.env.BUCKET_NAME; 
+    
+    record = JSON.parse(record.body).Records[0];
+    console.log(JSON.stringify(record));
 
-    try{
+    if (record.s3 && record.s3.object) {
+        const key = record.s3.object.key;
+
+        console.log(`bucketName: ${bucketName} and key: ${key}`);
         const data = await s3.getObject({
             Bucket: bucketName,
             Key: key
@@ -18,18 +27,7 @@ exports.handler = async (event) => {
         const csvData = data.Body.toString('utf-8');
         console.log(csvData);
         const emails =  csvData.split('\r\n');
-        console.log(emails);
-        return {
-            statusCode: 200,
-            body: 'Read CSV file'
-        }
-        
-    } catch(error) {
-        console.log("error============");
-        console.log(error);
-        return {
-            statusCode: 500,
-            body: `Error: ${error}`
-        }
-    }
+        console.log(emails);  
+    }    
+    
 }
